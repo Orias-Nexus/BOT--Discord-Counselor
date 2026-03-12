27215a19fdf970a15b1aea274be6330c2a56b5a6
Orias <long171.d.shine@gmail.com>
2026-03-12 11:17:57 +0700

    Enhance autopull.sh with improved logging and date management
    Updated logging messages to include timestamps for better tracking of deployment events. Added logic to clear the log file if the last recorded date differs from the current date, ensuring accurate log entries. Adjusted the AUTOPULL_LOG path for consistency with the PROJECT_DIR variable.

--- autopull.sh
+++ autopull.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+export TZ="Asia/Ho_Chi_Minh"
+
 PROJECT_DIR="/home/long1/JS--Discord-Counselor"
-AUTOPULL_LOG="/home/long1/JS--Discord-Counselor/autopull.log"
+AUTOPULL_LOG="$PROJECT_DIR/autopull.log"
 cd $PROJECT_DIR
+
+if [ -s "$AUTOPULL_LOG" ]; then
+    LAST_DATE=$(tail -n 1 "$AUTOPULL_LOG" | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}')
+    CURRENT_DATE=$(date +'%Y-%m-%d')
+    if [ "$LAST_DATE" != "" ] && [ "$CURRENT_DATE" != "$LAST_DATE" ]; then
+        : > "$AUTOPULL_LOG"
+    fi
+fi
+
 git fetch origin main > /dev/null 2>&1
 LOCAL=$(git rev-parse HEAD)
 REMOTE=$(git rev-parse @{u})
 if [ $LOCAL != $REMOTE ]; then
     : > $AUTOPULL_LOG
-    echo "[$(date)] NEW VERSION DETECTED. UPDATING..."
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] NEW VERSION DETECTED. UPDATING..."
     git pull origin main
     npm run install:all
     npm run prisma:generate:backend
@@ -19,9 +30,12 @@ if [ $LOCAL != $REMOTE ]; then
     pm2 flush
     rm -rf $PROJECT_DIR/private/logs/*
     pm2 restart all
-    echo "[$(date)] UPDATED SUCCESSFULLY."
-    # tree -I 'node_modules'
-    # pm2 logs
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] UPDATED SUCCESSFULLY."
 else
-    echo "[$(date)] CHECKED: No changes."
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] CHECKED: NO CHANGES."
 fi
+
+# tree -I 'node_modules'
+# crontab -e
+# pm2 logs
+# */1 * * * * /home/long1/JS--Discord-Counselor/autopull.sh >> /home/long1/JS--Discord-Counselor/autopull.log 2>&1
