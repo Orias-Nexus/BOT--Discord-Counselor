e9b7f1b3e376f0fc54ac295ea799c877dc2f50f2
Orias <long171.d.shine@gmail.com>
2026-03-16 12:43:39 +0700

    Refactor embed handling: introduce placeholder resolution for embeds in CategoryInfo, ChannelInfo, MemberInfo, ServerInfo, and update embed context utilities. Remove deprecated Greeting and Leaving embed templates to streamline embed management.

--- directive/src/embeds/index.js
+++ directive/src/embeds/index.js
@@ -1,8 +1,8 @@
 export { replaceInString, replacePlaceholders, omitNull, EMBED_COLORS } from './schema.js';
-export { getGreetingEmbed } from './Greeting.js';
-export { getLeavingEmbed } from './Leaving.js';
-export { getServerInfoEmbed } from './ServerInfo.js';
-export { getChannelInfoEmbed } from './ChannelInfo.js';
-export { getCategoryInfoEmbed } from './CategoryInfo.js';
-export { getMemberInfoEmbed } from './MemberInfo.js';
+export { getDefaultEmbedTemplate, getResolvedEmbedForDisplay, DEFAULT_EMBED_TEMPLATE } from './embedTemplate.js';
+export { resolveEmbed, resolveString, getEmbedParser, createEmbedContext } from './embedContext.js';
+export { getServerInfoEmbed } from './serverInfo.js';
+export { getChannelInfoEmbed } from './channelInfo.js';
+export { getCategoryInfoEmbed } from './categoryInfo.js';
+export { getMemberInfoEmbed } from './memberInfo.js';
 export { formatShortDate, getChannelTypeName, getChannelStatus } from './utils.js';
