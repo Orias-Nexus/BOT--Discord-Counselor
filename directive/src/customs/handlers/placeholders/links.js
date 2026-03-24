ed9a144ee3b606a8c771ef4bef8ab53cd65757a0
Orias <long171.d.shine@gmail.com>
2026-03-25 00:34:06 +0700

    Update banner and icon URLs in links.js to reflect new repository structure

--- directive/src/customs/handlers/placeholders/links.js
+++ directive/src/customs/handlers/placeholders/links.js
@@ -1,31 +1,31 @@
 export function blank_banner() {
-  return 'https://github.com/Orias1701/Resources--Discord-Bots/blob/main/assets/img/Banner-Blank.png?raw=true';
+  return 'https://github.com/Orias-Store/RES--Discord-Resource/blob/main/assets/img/Banner-Blank.png?raw=true';
 }
 
 export function imposter_banner() {
-  return 'https://github.com/Orias1701/Resources--Discord-Bots/blob/main/assets/img/Banner-Imposter.png?raw=true';
+  return 'https://github.com/Orias-Store/RES--Discord-Resource/blob/main/assets/img/Banner-Imposter.png?raw=true';
 }
 
 export function manycat_banner() {
-  return 'https://github.com/Orias1701/Resources--Discord-Bots/blob/main/assets/img/Banner-Manycat.png?raw=true';
+  return 'https://github.com/Orias-Store/RES--Discord-Resource/blob/main/assets/img/Banner-Manycat.png?raw=true';
 }
 
 export function nahihi_banner() {
-  return 'https://github.com/Orias1701/Resources--Discord-Bots/blob/main/assets/img/Banner-Nahihi.png?raw=true';
+  return 'https://github.com/Orias-Store/RES--Discord-Resource/blob/main/assets/img/Banner-Nahihi.png?raw=true';
 }
 
 export function whitepet_banner() {
-  return 'https://github.com/Orias1701/Resources--Discord-Bots/blob/main/assets/img/Banner-Whitepet.png?raw=true';
+  return 'https://github.com/Orias-Store/RES--Discord-Resource/blob/main/assets/img/Banner-Whitepet.png?raw=true';
 }
 
 export function paimom_icon() {
-  return 'https://github.com/Orias1701/Resources--Discord-Bots/blob/main/assets/img/Icon-Paimom.png?raw=true';
+  return 'https://github.com/Orias-Store/RES--Discord-Resource/blob/main/assets/img/Icon-Paimom.png?raw=true';
 }
 
 export function march71_icon() {
-  return 'https://github.com/Orias1701/Resources--Discord-Bots/blob/main/assets/img/Icon-March71.png?raw=true';
+  return 'https://github.com/Orias-Store/RES--Discord-Resource/blob/main/assets/img/Icon-March71.png?raw=true';
 }
 
 export function march72_icon() {
-  return 'https://github.com/Orias1701/Resources--Discord-Bots/blob/main/assets/img/Icon-March72.png?raw=true';
+  return 'https://github.com/Orias-Store/RES--Discord-Resource/blob/main/assets/img/Icon-March72.png?raw=true';
 }
